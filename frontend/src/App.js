c23fd9889a591329f43f50f9d1ce2fda2f7b7b4c
parthsujalshah <parthsujalshah@gmail.com>
2021-10-13 01:23:55 +0530

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import 'antd/dist/antd.css';
-import { Form, Checkbox, Select, Input, Button, Slider, Card, Modal, Spin } from 'antd';
+import { Form, Checkbox, Select, Input, Button, Slider, Card, Modal, Spin, PageHeader, Affix } from 'antd';
 import React from 'react';
 import axios from "axios";
 import { apiUrl } from "./constants";
@@ -73,131 +73,143 @@ function App() {
   };
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-        {
-          modalLoading ?
-            <Spin size="large" />
-            :
-            <div>
-              {
-                staying ?
-                  <div>
-                    <Lottie height={"400"} width={"400"} options={defaultOptionsStaying} isStopped={isStopped} isPaused={isPaused} />
-                    <h2>Employee is Staying</h2>
-                  </div>
-                  :
-                  <div>
-                    <Lottie height={"400"} width={"400"} options={defaultOptionsLeaving} isStopped={isStopped} isPaused={isPaused} />
-                    <h2>Employee is Leaving</h2>
-                  </div>
-              }
-            </div>
-        }
-      </Modal>
-      <Card style={{ backgroundColor: "#f5f5f5" }}>
-        <Form
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
+    <div>
+      <Affix>
+        <Card style={{ backgroundColor: "#e6fffb" }}>
+          <PageHeader
+            // className="site-page-header"
+            // onBack={() => null}
+            title="Employee Turnover Predictor"
+            subTitle="Predict wether the employee will continue in your company or not"
+          />
+        </Card>
+      </Affix>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+          {
+            modalLoading ?
+              <Spin size="large" />
+              :
+              <div>
+                {
+                  staying ?
+                    <div>
+                      <Lottie height={"400"} width={"400"} options={defaultOptionsStaying} isStopped={isStopped} isPaused={isPaused} />
+                      <h2>Employee is Staying</h2>
+                    </div>
+                    :
+                    <div>
+                      <Lottie height={"400"} width={"400"} options={defaultOptionsLeaving} isStopped={isStopped} isPaused={isPaused} />
+                      <h2>Employee is Leaving</h2>
+                    </div>
+                }
+              </div>
+          }
+        </Modal>
+        <Card style={{ backgroundColor: "#fafafa" }}>
+          <Form
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
 
-          <Form.Item name="satisfaction_level" label="Employee Statisfaction Percent">
-            <Slider
-              marks={{
-                0: '0',
-                20: '20',
-                40: '40',
-                60: '60',
-                80: '80',
-                100: '100',
+            <Form.Item name="satisfaction_level" label="Employee Statisfaction Percent">
+              <Slider
+                marks={{
+                  0: '0',
+                  20: '20',
+                  40: '40',
+                  60: '60',
+                  80: '80',
+                  100: '100',
+                }}
+              />
+            </Form.Item>
+
+            <Form.Item
+              label="Time Since Last Evaluation (in years)"
+              name="last_evaluation"
+              rules={[
+                {
+                  required: true,
+                  message: 'This field is Required!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Time Spent at Company (in years)"
+              name="time_spend_company"
+              rules={[
+                {
+                  required: true,
+                  message: 'This field is Required!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              name="department"
+              label="Department"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Select
+                placeholder="Select the Department of the Employee"
+                allowClear
+              >
+                <Option value="RandD">R and D</Option>
+                <Option value="hr">HR</Option>
+                <Option value="management">Management</Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item
+              name="salary"
+              label="Salary"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+            >
+              <Select
+                placeholder="Select the Salary Slab of the Employee"
+                allowClear
+              >
+                <Option value="salary_high">High</Option>
+                <Option value="salary_low">Low</Option>
+              </Select>
+            </Form.Item>
+
+            <Form.Item name="Work_accident" valuePropName="checked">
+              <Checkbox>Employee Been in a Work Accident</Checkbox>
+            </Form.Item>
+
+            <Form.Item name="promotion_last_5years" valuePropName="checked">
+              <Checkbox>Employee Promoted in the last 5 Years</Checkbox>
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
               }}
-            />
-          </Form.Item>
-
-          <Form.Item
-            label="Time Since Last Evaluation (in years)"
-            name="last_evaluation"
-            rules={[
-              {
-                required: true,
-                message: 'This field is Required!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            label="Time Spent at Company (in years)"
-            name="time_spend_company"
-            rules={[
-              {
-                required: true,
-                message: 'This field is Required!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="department"
-            label="Department"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Select
-              placeholder="Select the Department of the Employee"
-              allowClear
             >
-              <Option value="RandD">R and D</Option>
-              <Option value="hr">HR</Option>
-              <Option value="management">Management</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item
-            name="salary"
-            label="Salary"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Select
-              placeholder="Select the Salary Slab of the Employee"
-              allowClear
-            >
-              <Option value="salary_high">High</Option>
-              <Option value="salary_low">Low</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item name="Work_accident" valuePropName="checked">
-            <Checkbox>Employee Been in a Work Accident</Checkbox>
-          </Form.Item>
-
-          <Form.Item name="promotion_last_5years" valuePropName="checked">
-            <Checkbox>Employee Promoted in the last 5 Years</Checkbox>
-          </Form.Item>
-
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
     </div>
   );
 }
