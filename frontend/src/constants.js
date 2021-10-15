5e756fb1785e7ef2640707bd2a7dce24becbdce5
parthsujalshah <parthsujalshah@gmail.com>
2021-10-15 12:39:15 +0530

    temp

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
 // export const apiUrl = 'http://127.0.0.1:8000/';
-// export const apiUrl = 'http://172.31.149.195:8000';
+export const apiUrl = 'http://172.31.149.195:8000';
 // export const apiUrl = 'http://tempmail2.pythonanywhere.com/';
-export const apiUrl = 'https://employee-turnover-pred.herokuapp.com/';
+// export const apiUrl = 'https://employee-turnover-pred.herokuapp.com/';
